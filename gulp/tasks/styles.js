eaf686472d86b54e87831ea7b81b08e7c4a33815
Naveen <b.g.naveenkumarclk@gmail.com>
2018-10-09 13:34:13 +0530

    complted responsive toggle menu

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,12 +4,13 @@ let gulp = require("gulp"),
   cssvars = require("postcss-simple-vars"),
   nested = require("postcss-nested"),
   cssimport = require("postcss-import"),
-  mixins = require("postcss-mixins");
+  mixins = require("postcss-mixins"),
+  hexrgba = require("postcss-hexrgba");
 
 gulp.task("styles", () => {
   return gulp
     .src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssimport, mixins, cssvars, nested, hexrgba, autoprefixer]))
     .on("error", function(errorinfo) {
       console.log(errorinfo.toString());
       this.emit("end");
